30d71445fee488578f376addd871903b98dad1b5
bobclarke <bobmclarke@gmail.com>
2021-12-29 13:55:30 +0000

    update concurrency

--- concurrency/concurrency_channels.go
+++ concurrency/concurrency_channels.go
@@ -9,17 +9,18 @@ func main() {
 	ch := make(chan int)
 
 	go func() {
-		for x := 0; x < 3; x++ {
+		for x := 0; x < 5; x++ {
 			fmt.Printf("Sending %d\n", x)
 			ch <- x
 			time.Sleep(time.Second)
 		}
+		close(ch)
 	}()
 
-	for y := 0; y < 3; y++ {
+	for i := range ch {
+		_ = i // Ignore i
 		val := <-ch
-		fmt.Printf("Got %d\n", val)
-
+		fmt.Printf("Received %d\n", val)
 	}
 
 }
