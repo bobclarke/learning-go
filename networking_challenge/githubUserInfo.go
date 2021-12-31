b5d10fb2085658e633a57af4752ce832eede8c48
bobclarke <bobmclarke@gmail.com>
2021-12-31 15:56:34 +0000

    networking

---
+++ networking_challenge/githubUserInfo.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type User struct {
+	Name        string `json:"name"`
+	PublicRepos int    `json:"public_repos"`
+}
+
+func userInfo(login string) (*User, error) {
+	url := "https://api.github.com/users/" + login
+
+	resp, err := http.Get(url) //Get the JSON data
+	if err != nil {
+		log.Fatalf("ERROR: unable to call %s - %s", url, err)
+	}
+
+	var buf bytes.Buffer // Create a buffer
+
+	io.Copy(&buf, resp.Body) // copy the http response to this buffer
+
+	u := &User{} // Create an empty User instance - note, u is a pointer to this User instance
+
+	enc := json.NewDecoder(&buf) // Create a JSON decoder to decode our HTTP response
+
+	err = enc.Decode(u) // Decode the JSON response into the User struct
+	if err != nil {
+		return u, fmt.Errorf("Error")
+	} else {
+		return u, nil
+	}
+}
+
+func main() {
+	userDetails, err := userInfo("bobclarke")
+	if err != nil {
+		log.Fatalf("Error: %s\n", err)
+	}
+
+	fmt.Printf("User: %s - public repos: %d\n", userDetails.Name, userDetails.PublicRepos)
+}
